4c48da2d2d878219f28e5aa7359a38b991407aa8
knubbe <knubbe1982@gmail.com>
2020-04-08 09:29:10 +0200

    following

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,13 +1,13 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-	@foreach (range(1,10) as $index)
+	@foreach (auth()->user()->follows as $user)
 		<li class="mb-4">
 			<div class="flex items-center text-sm">
 				<img 
-					src="https://i.pravatar.cc/40"
+					src="{{ $user->avatar }}"
 					class="rounded-full mr-2"
 				>
-				John Doe
+				{{ $user->name }}
 			</div>
 		</li>
 	@endforeach

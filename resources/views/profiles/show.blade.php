869318ea966faee1e364e99ea6c3d828d7455773
knubbe <knubbe1982@gmail.com>
2021-07-21 23:01:20 +0200

    wrap edit profile and follow button with check user

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -16,23 +16,29 @@
             >
         </div>
 
-	    <div class="flex justify-between items-center mb-6">
-	    	<div>
-	    		<h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
-	    		<p class="text-sm">{{ $user->created_at->diffForHumans() }}</p>
-	    	</div>
+        <div class="flex justify-between items-center mb-6">
+            <div>
+                <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
+                <p class="text-sm">{{ $user->created_at->diffForHumans() }}</p>
+            </div>
 
-	    	<div class="flex">
-	    		<a href="" class="rounded-full border border-gray-300 p-2 px-4 text-black text-xs mr-2">
-	    			Edit Profile
-	    		</a>
+            <div class="flex">
+                @if(auth()->user()->is($user))
+                    <a href="" class="rounded-full border border-gray-300 p-2 px-4 text-black text-xs mr-2">
+                        Edit Profile
+                    </a>
+                @endif
                 <x-follow-button :user="$user"></x-follow-button>
-	    	</div>
-    	</div>
+            </div>
+        </div>
 
-    	<p class="text-sm">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-		</p>
+        <p class="text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+        </p>
 
     </header>
 

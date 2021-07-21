0ffc2e199f5e04d711a87812150f200858a2d937
knubbe <knubbe1982@gmail.com>
2021-07-21 23:09:38 +0200

    extend path method with append

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -24,7 +24,7 @@
 
             <div class="flex">
                 @if(current_user()->is($user))
-                    <a href="" class="rounded-full border border-gray-300 p-2 px-4 text-black text-xs mr-2">
+                    <a href="{{ $user->path('edit') }}" class="rounded-full border border-gray-300 p-2 px-4 text-black text-xs mr-2">
                         Edit Profile
                     </a>
                 @endif
